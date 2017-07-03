1742f32132beccd1f0c9c67a2d9276a929abba25
cperceful <cpercef1@gmail.com>
2017-07-02 20:42:46 -0500

    Started class 2 prep

---
+++ Area/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//Calculate the area of a circle based on user input. A = pi * r * r
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What up? Let's calculate the area of a circle");
+            while (true)
+            {
+                Console.WriteLine("Enter the radius of the circle: ");
+                double radius = Double.Parse(Console.ReadLine());
+
+                if (radius < 0)
+                {
+                    continue;
+                } else
+                {
+                    double area = radius * radius * Math.PI;
+                    Console.WriteLine("The area of the circle is: " + area);
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> list = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.Next(0, 20));
+            }
+
+            foreach (int thing in list)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("The sum of all the numbers is: " + sum(list));
+            Console.ReadLine();
+            
+         
+        }
+
+        static int sum(List<int> list)
+        {
+            int sum = 0;
+
+            foreach (int item in list)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
