bfe4f7c7511a3c6c41808a27e9c7b0efe152e978
cperceful <cpercef1@gmail.com>
2017-07-08 16:59:01 -0500

    Created project and class files for Restaurant studio

---
+++ Student/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    class Course
+    {
+        private string name;
+        private int creditsWorth;
+        private List<Student> listOfStudents;
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    class Student
+    {
+
+        private string name;
+        private int studentId;
+        private int numberOfCredits;
+        private double gpa;
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public int StudentId
+        {
+            get { return studentId; }
+
+            internal set { studentId = value; }
+        }
+
+        public int NumberOfCredits
+        {
+            get; set;
+        }
+
+        public double Gpa
+        {
+            get; set;
+        }
+
+    }
+}
